82fef517bd1ec1c42a1351cd90d0d43e5270f99c
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-12-10 22:40:20 -0300

    primeira pagina ok

---
+++ src/js/mainpage.js
@@ -0,0 +1,107 @@
+import {useState, useEffect} from 'react';
+import React from 'react';
+import styled from "styled-components";
+import axios from "axios";
+import ReactDOM from "react-dom";
+//import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+
+export default function MainPage() {
+    const [movieslist, setMovieslist] = React.useState(undefined)
+
+    useEffect(() => {
+		const request = axios.get("https://mock-api.driven.com.br/api/v8/cineflex/movies");
+
+		request.then(resposta => {
+		    setMovieslist(resposta.data);
+		});
+        console.log(movieslist)
+	}, []);
+
+    if(movieslist === undefined){
+        console.log("carregando")
+        return 
+    }
+
+    return (
+    <>
+        <Header><p>Cineflex</p></Header>
+        <PageContent>
+            <SelectMovies><p>Selecione o filme</p></SelectMovies>
+        
+        <MoviesDiv>
+            {movieslist.map((movie) => 
+            <Movies key={movie.id}>
+                <MoviePoster src={movie.posterURL} alt={movie.title}/>
+            </Movies>     )}
+                 
+        </MoviesDiv>
+        </PageContent>
+    </>
+
+    )
+}
+
+const Header = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 67px;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    Font-family: Roboto;
+    Font-style: Regular;
+    Font-size: 34px;
+    Line-height: 40px;
+    Line-height: 100%;`
+
+
+const SelectMovies =styled.div`
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    width: 100%;
+    height: 110px;
+    color: #293845;
+    Font-family: Roboto;
+    Font-style: Regular;
+    Font-size: 24px;
+    Line-height: 28px;
+    Line-height: 100%;`
+
+const PageContent = styled.div`
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 67px;
+    background-color: #E5E5E5`
+const MoviesDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    flex-basis: 100%;`
+
+const Movies = styled.div`
+    display: flex;
+    flex: wrap;
+    justify-content: center;
+    align-items: center;
+    width: 145px;
+    min-Width: 145px;
+    Height: 209px;
+    background-color: #ffffff;
+    margin-left: 15px;
+    margin-top: 20px;
+    margin-right: 15px;
+    margin-bottom: 20px;
+    box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;`
+const MoviePoster = styled.img`
+    display: flex;
+    width: 129px;
+    height: 193px;`
